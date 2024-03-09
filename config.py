0190196a456113c55b12e09e0faa2ec92b024029
bhuriya12 <162349127+bhuriya12@users.noreply.github.com>
2024-03-09 23:59:55 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+    AUTH_USERS= int(os.environ.get("AUTH_USERS", 0))
+
